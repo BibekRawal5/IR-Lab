b0c07e7af36f27ae55bd34c24729c3a3bfdadb09
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-08-28 20:45:45 +0545

    Implement link extraction from URL

--- br-extract-links.py
+++ br-extract-links.py
@@ -1,1 +1,16 @@
 
+import urllib.request
+import re
+
+def extract_links(url, outfile):
+    response = urllib.request.urlopen(url)
+    html = response.read().decode("utf-8")
+    links = re.findall(r'href=["\'](.*?)["\']', html)
+    with open(outfile, "w", encoding="utf-8") as f:
+        for link in links:
+            f.write(link + "\n")
+    print("Total links found:", len(links))
+
+url = "https://bibek.rawal.com.np"
+outfile = "br-links.txt"
+extract_links(url, outfile)
