66bb3b4f0c61fd62efc5c48c7089ceda8194cf12
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-08-28 20:46:28 +0545

    Implement file reading and word counting function

--- br-lab1.py
+++ br-lab1.py
@@ -1,1 +1,9 @@
+def read_file(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read()
+    words = text.split()
+    print("File Content:\n", text)
+    print("Total Words:", len(words))
 
+file = "sample.txt"
+read_file(file)
