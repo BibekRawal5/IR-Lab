3820d35618b7309d3f18d0f4cb5775d4e7fe22ed
Bibek Rawal <102865170+BibekRawal5@users.noreply.github.com>
2025-08-28 20:48:08 +0545

    Implement term frequency calculation in br-lab3.py

--- br-lab3.py
+++ br-lab3.py
@@ -1,1 +1,14 @@
 
+def term_frequency(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.read().lower()
+    words = text.split()
+    freq = {}
+    for w in words:
+        freq[w] = freq.get(w, 0) + 1
+    print("Term Frequency:")
+    for word, count in freq.items():
+        print(word, ":", count)
+
+file = "sample.txt"
+term_frequency(file)
